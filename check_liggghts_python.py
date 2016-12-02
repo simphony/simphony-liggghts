e58e2fe53b198305ee7850f29662a9f4d54f053a
Stefano Borini <sborini@enthought.com>
2016-12-02 10:02:48 +0000

    Debugging why the module is not correctly imported

--- check_liggghts_python.py
+++ check_liggghts_python.py
@@ -1,3 +1,8 @@
+import sys
+import site
+print site.USER_SITE
+print sys.path
+
 import liggghts
 l = liggghts.liggghts(cmdargs=["-log", "none"])
 l.command("print \"Python-LIGGGHTS interface is working\"")
